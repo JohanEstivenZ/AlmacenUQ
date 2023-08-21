66f5e68b323450fa698ee63ca4352dc25b853824
JohanEstivenZ <johane.zapataa@uqvirtual.edu.co>
2023-08-21 17:22:20 -0500

    cambio

--- src/Controlador/ControladorProducto.java
+++ src/Controlador/ControladorProducto.java
@@ -8,11 +8,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ControladorProducto {
+public class ControladorProducto implements Initializable{
     @javafx.fxml.FXML
     private TextField txtNombreProducto;
     @javafx.fxml.FXML
@@ -44,12 +48,25 @@ public class ControladorProducto {
     @javafx.fxml.FXML
     private TextField txtTemperaturaRefigeramientoProducto;
     @javafx.fxml.FXML
-    private Label labelTemperatura1;
-    @javafx.fxml.FXML
     private DatePicker dateFechaVencimiento;
+    @FXML
+    private Label lavelFechaEnvasado;
+    @FXML
+    private Label labelFechaVencimineto;
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-
-
+    }
+    private void handleClienteNaturalSelected(ActionEvent event) {
+        boolean isSelected = RdoClienteNatural.isSelected();
+        // Ocultar o mostrar las opciones relacionadas con el cliente natural según la selección
+        labelCodigoAprovacion.setVisible(!isSelected);
+        txtCodigoAprovacionProducto.setVisible(!isSelected);
+        labelTemperatura.setVisible(!isSelected);
+        txtTemperaturaRefigeramientoProducto.setVisible(!isSelected);
+        dateFechaVencimiento.setVisible(!isSelected);
+        labelFechaVencimineto.setVisible(!isSelected);
+    }
 }
